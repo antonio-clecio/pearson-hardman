e8bde8d5bf2717608d93c517f4eeec4b0e713594
Antonio Clecio <clecio_devops@outlook.com>
2022-11-10 22:30:20 -0300

    third commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// Seleção de elementos
+const menuBtn = document.querySelector("#menu");
+const closeMenuBtn = document.querySelector("#close-menu");
+const menu = document.querySelector("#mobile-navbar");
+
+const desktopLinks = document.querySelectorAll("#navbar a");
+const mobileLinks = document.querySelectorAll("#mobile-navbar a");
+const allLinks = [...desktopLinks, ...mobileLinks];
+
+console.log(allLinks);
+
+// Funções
+function smoothScroll(e) {
+    e.preventDefault();
+
+    const href = this.getAttribute("href");
+    const offsetTop = document.querySelector(href).offsetTop;
+
+    scroll({
+        top: offsetTop,
+        behavior: "smooth",
+    });
+}
+
+// Eventos
+[menuBtn, closeMenuBtn].forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        menu.classList.toggle("menu-active");
+    });
+});
+
+allLinks.forEach((link) => {
+
+    link.addEventListener("click", smoothScroll);
+});
